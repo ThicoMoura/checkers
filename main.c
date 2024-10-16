6c0d5a9e92a5e1ddd0861ffb647744a65b2b4f8c
Thiago F. Moura <moura.francisco.thiago@hotmail.com>
2024-10-16 13:34:05 -0400

    Create func for move piece

--- main.c
+++ main.c
@@ -32,8 +32,12 @@ typedef struct {
     int y;
 } Pos;
 
+void cls();
 void newGame(Game *);
-void printBoard(Game *);
+void printBoard(Game);
+void gameStatus(Game *);
+Pos readPos();
+int validPosition(Game *, Pos);
 int validMove(Game *, Pos, Pos);
 int move(Game *, Pos, Pos);
 
@@ -41,16 +45,37 @@ int main(int argc, char ** argv) {
     Game game;
 
     newGame(&game);
-    printBoard(&game);
 
-    // Test move
-    Pos start = {1, 5};
-    Pos end = {3, 3};
+    while (game.Status == 0) {
+        cls();
+        printBoard(game);
+        Pos start = readPos();
+        if (!validPosition(&game, start)) {
+            printf("Invalid piece! Please, select a valid piece\n");
+            continue;
+        }
 
-    move(&game, start, end);
-    printBoard(&game);
+        cls();
+        printBoard(game);
+        Pos end;
+        do {
+            end = readPos();
+            if (validMove(&game, start, end)) break;
+            printf("Invalid move! Please, select a valid move\n");
+        } while(1);
+
+        move(&game, start, end);
+    }
 
     return 0;
+}
+
+void cls() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        printf("\e[1;1H\e[2J");
+    #endif
 }
 
 void newGame(Game * game) {
@@ -71,14 +96,14 @@ void newGame(Game * game) {
     (*game).Status = GAMMING;
 }
 
-void printBoard(Game * game) {
+void printBoard(Game game) {
     printf("   A   B   C   D   E   F   G   H\n");
     printf(" +---+---+---+---+---+---+---+---+\n");
     for (int i = MIN; i < MAX; i++) {
         printf("%d|", 8 - i);
         for (int j = MIN; j < MAX; j++) {
-            if ((*game).Board[i][j] == WHITE) printf(" O |");
-            else if((*game).Board[i][j] == BLACK) printf(" 0 |");
+            if (game.Board[i][j] == WHITE) printf(" O |");
+            else if(game.Board[i][j] == BLACK) printf(" 0 |");
             else printf("   |");
         }
         printf("\n");
@@ -87,19 +112,85 @@ void printBoard(Game * game) {
     printf("   A   B   C   D   E   F   G   H\n");
 }
 
+Pos readPos() {
+    char * pos;
+
+    pos = malloc(2 * sizeof (char *));
+
+    do {
+        printf("Insert position: ");
+        scanf("%s", pos);
+        if(
+            ((pos[0] >= 'A' && pos[0] <= 'H') || (pos[0] >= 'a' && pos[0] <= 'h')) &&
+            (pos[1] >= '1' && pos[1] <= '8')
+        )
+            return (Pos) { .x = ((int)(pos[0]) - ((pos[0] >= 'A' && pos[0] <= 'H') ? 'A' : 'a')), .y = (int)(pos[1]) - '0'};
+        else 
+            printf("Invalid position, please, insert a valid position!\n");
+    } while (1); 
+}
+
+int validPosition(Game* game, Pos pos) {
+    if ((*game).Board[pos.y][pos.x] != EMPTY)
+        if ((*game).Board[pos.y][pos.x] == (*game).CurrentPlayer)
+            if (
+                ((*game).CurrentPlayer == BLACK && pos.y > 0 && 
+                    ( pos.x > 0 && (*game).Board[pos.y - 1][pos.x - 1] == EMPTY) ||
+                    ( pos.x < 7 && (*game).Board[pos.y - 1][pos.x + 1] == EMPTY) ||
+                    ( pos.x > 1 && pos.y > 1 && (*game).Board[pos.y - 1][pos.x - 1] == WHITE && (*game).Board[pos.y - 2][pos.x - 2] == EMPTY) ||
+                    ( pos.x > 1 && pos.y > 1 && (*game).Board[pos.y - 1][pos.x + 1] == WHITE && (*game).Board[pos.y - 2][pos.x + 2] == EMPTY)
+                ) ||
+                ((*game).CurrentPlayer == WHITE && pos.y < 7 && 
+                    ( pos.x > 0 && (*game).Board[pos.y + 1][pos.x - 1] == EMPTY) ||
+                    ( pos.x < 7 && (*game).Board[pos.y + 1][pos.x + 1] == EMPTY) ||
+                    ( pos.x > 1 && pos.y < 6 && (*game).Board[pos.y + 1][pos.x - 1] == BLACK && (*game).Board[pos.y + 2][pos.x - 2] == EMPTY) ||
+                    ( pos.x > 1 && pos.y < 6 && (*game).Board[pos.y + 1][pos.x + 1] == BLACK && (*game).Board[pos.y + 2][pos.x + 2] == EMPTY)
+                )
+            ) return 0;
+    return 1;
+}
+
 int validMove(Game* game, Pos start, Pos end) {
     if (((*game).Board[start.y][start.x] == (*game).CurrentPlayer)) {
-        if((*game).CurrentPlayer == WHITE && (end.y == (start.y + 2) && (end.x == (start.x - 2) || end.x == (start.x + 2)))) return 1;
-        if((*game).CurrentPlayer == BLACK && (end.y == (start.y - 2) && (end.x == (start.x - 2) || end.x == (start.x + 2)))) return 1;
+        if (
+            (*game).CurrentPlayer == WHITE && 
+            (end.y == (start.y + 1) && (end.x == (start.x + 1) || end.x == (start.x - 1)) && ((*game).Board[end.y][end.x] == EMPTY))
+        ) return 0;
+        if (
+            (*game).CurrentPlayer == BLACK && 
+            (end.y == (start.y - 1) && (end.x == (start.x + 1) || end.x == (start.x - 1)) && ((*game).Board[end.y][end.x] == EMPTY))
+        ) return 0;
+        if(
+            (*game).CurrentPlayer == WHITE && 
+            (end.y == (start.y + 2) &&
+                (
+                    (end.x == (start.x - 2) && (*game).Board[start.y + 1][start.x - 1] == BLACK && (*game).Board[end.y][end.x] ==  EMPTY) || 
+                    (end.x == (start.x + 2) && (*game).Board[start.y + 1][start.x + 1] == BLACK && (*game).Board[end.y][end.x] == EMPTY)
+                )
+            )
+        ) return 0;
+        if(
+            (*game).CurrentPlayer == BLACK && 
+            (end.y == (start.y - 2) &&
+                (
+                    (end.x == (start.x - 2) && (*game).Board[start.y - 1][start.x - 1] == WHITE && (*game).Board[end.y][end.x] ==  EMPTY) || 
+                    (end.x == (start.x + 2) && (*game).Board[start.y - 1][start.x + 1] == WHITE && (*game).Board[end.y][end.x] == EMPTY)
+                )
+            )
+        ) return 0;
     }
-    return 0;
+    return 1;
 }
 
 int move(Game * game, Pos start, Pos end) {
-    if (validMove(game, start, end)) {
+    if (!validMove(game, start, end)) {
         int swap = (*game).Board[start.y][start.x];
         (*game).Board[start.y][start.x] = 0;
         (*game).Board[end.y][end.x] = swap;
+        if (((*game).CurrentPlayer == BLACK && start.y > (end.y + 1)) || ((*game).CurrentPlayer == WHITE && start.y < (end.y - 1)))
+            (*game).Board[end.y + ((end.y - start.y) < 0 ? +1 : - 1)][end.x + ((end.x - start.x) < 0 ? +1 : - 1)] = EMPTY;
+        (*game).CurrentPlayer = (*game).CurrentPlayer == PLAYER_ONE ? PLAYER_TWO : PLAYER_ONE;
+        return 1;
     }
-    return 1; 
+    return 0; 
 }
